fde47aa199d6f6d2191c5cd04fe0bbffd92c63f8
hitcherland <hitcherland@gmail.com>
2020-09-06 23:44:08 +0100

    Updates

---
+++ precache-manifest.5c0987d31c9103c0195c55aca529b248.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92cd872b223e63b37a39e0a39a1f1f5",
+    "url": "/village-generator/index.html"
+  },
+  {
+    "revision": "7e759a545bceb25db472",
+    "url": "/village-generator/static/css/main.1b1ace7a.chunk.css"
+  },
+  {
+    "revision": "c14136a78e637b4b8b52",
+    "url": "/village-generator/static/js/2.6a69683a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/village-generator/static/js/2.6a69683a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e759a545bceb25db472",
+    "url": "/village-generator/static/js/main.3df8cf72.chunk.js"
+  },
+  {
+    "revision": "f299e1d492ce59d75e3c",
+    "url": "/village-generator/static/js/runtime-main.1c874e4c.js"
+  },
+  {
+    "revision": "d2ba8fea7e17c28fa7dfe13b3bbbdee1",
+    "url": "/village-generator/static/media/CNA21.d2ba8fea.px"
+  },
+  {
+    "revision": "c625e72fa66b68ea0a8c1f8b73d7d2a1",
+    "url": "/village-generator/static/media/VSA50.c625e72f.px"
+  },
+  {
+    "revision": "0dc412dfb915940c9e3adbed85efcd32",
+    "url": "/village-generator/static/media/VSA60.0dc412df.px"
+  }
+]);
